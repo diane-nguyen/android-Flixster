0929f140f655197bd34b1d2b74ddee905006c5e3
Diane <diane.nguyen.124@gmail.com>
2021-02-19 22:09:03 -0800

    Fixed posterview bug

--- app/src/main/java/com/dianenguyen/flixster/models/Movie.java
+++ app/src/main/java/com/dianenguyen/flixster/models/Movie.java
@@ -29,7 +29,7 @@ public class Movie {
 
     public String getPosterPath() {
     //  hard code size
-        return String.format("https://image.tmdb.org/t/p/w342/%s)", posterPath);
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
     public String getTitle() {
